18ffa3f3e4a56eaad11ac3db66a08efa561ab6cd
Tyson Wu <wukwanyutyson@gmail.com>
2022-04-15 04:03:51 +0800

    plotting styles

--- plotting.py
+++ plotting.py
@@ -1,9 +1,10 @@
 from datetime import datetime, timezone
-from pytest import Instance
 import pytz
 
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+import pyqtgraph as pg
 
 import finplot_lib as fplt
 
@@ -24,23 +25,148 @@ if __name__ == '__main__':
     
     # slice_dt = pytz.timezone('Asia/Hong_Kong').localize(datetime(2022,3,17,17,23,0)) # input in HKT
     slice_start = pytz.timezone('Asia/Hong_Kong').localize(datetime(2021,12,10,0,0,0)) # input in HKT
-    slice_end = pytz.timezone('Asia/Hong_Kong').localize(datetime(2021,12,12,0,0,0)) # input in HKT
+    slice_end = pytz.timezone('Asia/Hong_Kong').localize(datetime(2021,12,10,16,0,0)) # input in HKT
     
     # mp_slice = profile[slice_dt]
     mp_slice = profile[slice_start:slice_end]
 
-    ochl = pd.DataFrame(
+    ohlcv = pd.DataFrame(
         {
             'o': [mp.open for mp in mp_slice],
-            'c': [mp.close for mp in mp_slice],
             'h': [mp.high for mp in mp_slice],
             'l': [mp.low for mp in mp_slice],
-            # 'v': [mp.volume_qty for mp in mp_slice]
+            'c': [mp.close for mp in mp_slice],
+            'v': [mp.volume_qty for mp in mp_slice],
+            'd': [mp.delta_qty for mp in mp_slice]
         },
         index=[mp.timepoint for mp in mp_slice]
     )
     
-    # ohlcv.plot(y='o')
-    # plt.show()
-    fplt.candlestick_ochl(datasrc=ochl, candle_width=0.2)
+    # plotting
+    fplt.foreground = '#D6DBDF'
+    fplt.background = '#151E26'
+    ax, ax2 = fplt.create_plot(
+        title='Orderflow',
+        rows=2, # row1: candlestick, volume; row2: delta table; row2:
+    )
+
+    # set font
+    '''
+    TODO: set font
+    '''
+
+    # fplt.windows[0].ci.layout.setRowStretchFactor(0, 8) # make primary plot large, and implicitly table small
+
+    # add candlestick
+    candlestick_plot = fplt.candlestick_ochl(datasrc=ohlcv[['o', 'c', 'h', 'l']], candle_width=0.6, ax=ax)
+    
+    # add volume
+    volume_plot = fplt.volume_ocv(ohlcv[['o', 'c', 'v']], ax=ax.overlay())
+
+    # add delta table
+    '''
+    Ref: examples/bubble-table.py
+    '''
+    # def skip_y_crosshair_info(x, y, xt, yt): # we don't want any Y crosshair info on the table
+    #     return xt, ''
+    # ax2.set_visible(yaxis=False)
+    # fplt.add_crosshair_info(skip_y_crosshair_info, ax=ax2)
+    # fplt.set_y_range(0, 2, ax2)
+    # # hmmm it is weird that finplot requires the column to be visualized to be named int 0,1,2,...
+    # delta_df = ohlcv.copy()
+    # delta_df[1] = delta_df['d']
+    # delta_df[0] = delta_df['d'].cumsum()
+    
+    # colmap = fplt.ColorMap([0.0, 0.5, 1.0], [[200, 80, 60], [200, 190, 100], [40, 170, 30]]) # traffic light colors
+    # ts = [int(t.timestamp()) for t in ohlcv.index]
+    # fplt.heatmap(delta_df[[1, 0]], colmap=colmap, colcurve=lambda x: x, ax=ax2)
+    # fplt.labels(ts, [1.5] * len(ohlcv), [f'{d:2f}' for d in ohlcv['d']], ax=ax2, anchor=(0.5, 0.5))
+    # fplt.labels(ts, [0.5] * len(ohlcv), [f'{d:2f}' for d in ohlcv['d'].cumsum()], ax=ax2, anchor=(0.5, 0.5))
+
+    '''
+    Ref: examples/snp500.py
+    '''
+    # plot cvd
+    line_color = '#3498DB'
+    cvd_plot = fplt.plot(np.cumsum(ohlcv['d']), ax=ax2, legend='CVD', color=line_color, fillLevel=0, brush=line_color+'10')
+    # and set background
+    vb = cvd_plot.getViewBox()
+    vb.setBackgroundColor('#00000000')
+
+    '''
+    Ref: examples/complicated.py
+    '''
+    # set bull body to same color as bull frame; otherwise it is default background color (transparent)
+    bull = '#1ABC9C'
+    bear = '#E74C3C'
+    fplt.candle_bull_color = bull
+    fplt.candle_bull_body_color = bull
+    fplt.candle_bear_color = bear
+    candlestick_plot.colors.update({
+        'bull_body': fplt.candle_bull_color
+    })
+
+    transparency = '45'
+    volume_plot.colors.update({
+        'bull_frame': fplt.candle_bull_color + transparency, 
+        'bull_body': fplt.candle_bull_body_color + transparency,
+        'bear_frame': fplt.candle_bear_color + transparency, 
+        'bear_body': fplt.candle_bear_color + transparency,
+    })
+    
+    # set gridlines
+    ax.showGrid(x=True, y=True, alpha=0.2)
+    ax2.showGrid(x=True, y=True, alpha=0.2)
+    
+    # add legend of ohlcv data
+    '''
+    Ref: examples/snp500.py
+    '''
+    hover_label = fplt.add_legend('', ax=ax)
+    def update_legend_text(x, y):
+        dt = datetime.fromtimestamp(x // 1000000000)
+        utcdt = dt.astimezone(pytz.utc)
+        # dt = dt.replace(tzinfo=timezone.utc)
+        row = ohlcv.loc[utcdt]
+        # format html with the candle and set legend
+        fmt = '<span style="color:%s; margin: 16px;">%%s</span>' % (bull if (row['o'] < row['c']).all() else bear)
+        rawtxt = '<span style="font-size:14px">%%s %%s</span> &nbsp; O: %s H: %s L: %s C: %s Delta: %s' % (fmt, fmt, fmt, fmt, fmt)
+        hover_label.setText(rawtxt % ('TOKEN', 'INTERVAL', row['o'], row['h'], row['l'], row['c'], row['d']))
+    fplt.set_time_inspector(update_legend_text, ax=ax, when='hover')
+    
+    # additional crosshair info
+    def enrich_info(x, y, xtext, ytext):
+        o = ohlcv.iloc[x]['o']
+        h = ohlcv.iloc[x]['h']
+        l = ohlcv.iloc[x]['l']
+        c = ohlcv.iloc[x]['c']
+        add_yt = f'\tOpen: {o}\n\tHigh: {h}\n\tLow: {l}\n\tClose: {c}'
+        return xtext, add_yt
+    
+    fplt.add_crosshair_info(enrich_info, ax=ax)
+
+    # set dark themes ====================
+    pg.setConfigOptions(foreground=fplt.foreground, background=fplt.background)
+
+    # window background
+    for win in fplt.windows:
+        win.setBackground(fplt.background)
+
+    # axis, crosshair, candlesticks, volumes
+    axs = [ax for win in fplt.windows for ax in win.axs]
+    vbs = set([ax.vb for ax in axs])
+    axs += fplt.overlay_axs
+    axis_pen = fplt._makepen(color=fplt.foreground)
+    for ax in axs:
+        ax.axes['left']['item'].setPen(axis_pen)
+        ax.axes['left']['item'].setTextPen(axis_pen)
+        ax.axes['bottom']['item'].setPen(axis_pen)
+        ax.axes['bottom']['item'].setTextPen(axis_pen)
+        if ax.crosshair is not None:
+            ax.crosshair.vline.pen.setColor(pg.mkColor(fplt.foreground))
+            ax.crosshair.hline.pen.setColor(pg.mkColor(fplt.foreground))
+            ax.crosshair.xtext.setColor(fplt.foreground)
+            ax.crosshair.ytext.setColor(fplt.foreground)
+    # ====================================
+
     fplt.show()
